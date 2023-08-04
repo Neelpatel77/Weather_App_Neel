c487679055f69776edf393d40a6d523dc6f306c2
Neel Patel <94647177+Neelpatel77@users.noreply.github.com>
2023-08-04 17:12:38 -0400

    Update SceneDelegate.swift
    Update SceneDelegate.swift

--- Weather_Project_Neel/Weather_Project_Neel/SceneDelegate.swift
+++ Weather_Project_Neel/Weather_Project_Neel/SceneDelegate.swift
@@ -4,12 +4,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        let viewController = ViewController()
-        let navigationController = UINavigationController(rootViewController: viewController)
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
-    }
+func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    window = UIWindow(windowScene: windowScene)
+    let viewController = ViewController()
+    let navigationController = UINavigationController(rootViewController: viewController)
+    window?.rootViewController = navigationController
+    window?.makeKeyAndVisible()
+}
+
 }
